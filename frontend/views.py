a0eecd3787ee0ec55ad5ee3d865979b66f39d2d7
Salahuddin Shaikh <33448178+salahoddin88@users.noreply.github.com>
2022-01-15 13:57:18 +0530

    template configuration

---
+++ frontend/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views import View
+from product.models import ProductCategory
+
+
+class HomePage(View):
+
+    def get(self, request):
+        navigationProductCategory = ProductCategory.objects.filter(status=True)
+
+        # productCategory = ProductCategory.objects.filter(status=True)[start:end-1]
+        productCategory = ProductCategory.objects.filter(status=True).order_by('-id')[:3]
+        print(productCategory)
+        context = {
+            'navigationProductCategory' : navigationProductCategory,
+            'productCategory' : productCategory
+        }
+
+
+
+        return render(request, 'home-page.html', context)
+
+
